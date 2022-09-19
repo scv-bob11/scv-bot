27913c54350eb230fbac141ec2537963eeca90b1
D4m0n <d4m0n.8@gmail.com>
2022-09-19 21:59:20 +0900

    filter added

--- stream.py
+++ stream.py
@@ -5,4 +5,5 @@ bot = TwitterBot()
 
 
 bot.add_rules('bounty live (defi OR bug OR "$" OR swap OR token OR eth OR nft OR dao OR dex OR finance) min_faves:5')
+bot.add_rules('upgraded (defi OR bug OR "$" OR swap OR token OR eth OR nft OR dao OR dex OR finance) min_faves:5')
 bot.stream_filter()
